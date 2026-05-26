ee6e74cca32a9a37cb4415f41ed1845995c6daea
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2026-05-26 02:49:09 +0200

    modify constructor of Companion manager for storage

--- ide_companion/companion-sonion/src/companion/CompanionManager.ts
+++ ide_companion/companion-sonion/src/companion/CompanionManager.ts
@@ -1,11 +1,27 @@
+import * as vscode from 'vscode';
 import { Companion } from './Companion';
+
+const STORAGE_KEY = 'companions';
 
 export class CompanionManager {
 
     private companions: Map<string, Companion> = new Map();
 
+    constructor(private storage?: vscode.Memento) {
+        if (this.storage) {
+            const saved = this.storage.get<Companion[]>(STORAGE_KEY, []);
+            saved.forEach(c => this.companions.set(c.id, c));
+        }
+    }
+
+    private save() {
+        if (!this.storage) return;
+        this.storage.update(STORAGE_KEY, Array.from(this.companions.values()));
+    }
+
     create(companion: Companion) {
         this.companions.set(companion.id, companion);
+        this.save();
     }
 
     update(id: string, data: Partial<Companion>) {
@@ -13,10 +29,16 @@ export class CompanionManager {
         if (!c) return;
 
         this.companions.set(id, { ...c, ...data });
+        this.save();
     }
 
     delete(id: string) {
         this.companions.delete(id);
+        this.save();
+    }
+
+    get(id: string) {
+        return this.companions.get(id);
     }
 
     getAll() {
